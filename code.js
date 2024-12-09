429c7c05cd8f72220017a8391dad1b03f17f3830
scholtenskyle <35465569+scholtenskyle@users.noreply.github.com>
2024-12-08 17:56:30 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
 function e(n) {
     let addition = 0;
-    let fact = 1;
+    let factorial = 1;
     for (let i = 0; i <= n; i++) {
         if (i > 0) {
             fact *= i;
         }
-        addition += (1.0 / fact);
+        addition += (1.0 / factorial);
     }
 return addition;
 }
