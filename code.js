20e271c2e2ecbbc42d9e0c18b3c1c16a5adf9745
scholtenskyle <35465569+scholtenskyle@users.noreply.github.com>
2024-12-08 17:54:49 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
-function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
-}
-
 function e(n) {
-    if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+    for (let i = 0; i <= n; i++) {
+        if (i > 0) {
+            factorial *= i;
+        }
+        sum += (1.0 / factorial);
+    }
+return sum;
 }
